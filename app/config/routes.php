8ecd1df42e777f28d463078f9e9f3f73432fc9bd
Trisha Denisse Bundalian <trishadenissebundalian842@gmail.com>
2026-08-19 22:15:32 +0800

    Add student home root route

--- app/config/routes.php
+++ app/config/routes.php
@@ -46,6 +46,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->get('/student', 'StudentController::index');
 
+$router->get('/student', 'StudentController::index');
 
 $router->get('/student/profile', 'StudentController::profile')
     ->middleware('StudentMiddleware');
