71be24bc4b06295dabb59fffa7795c7d4366cb8b
Trisha Denisse Bundalian <trishadenissebundalian842@gmail.com>
2026-09-10 23:01:33 +0800

    Fix Render database environment variables

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = [
     'driver'   => 'mysql',
-    'hostname' => '143.244.134.90',
-    'port'     => '17754',
-    'username' => 'avnadmin',
-    'password' => '',
-    'database' => 'defaultdb',
+    'hostname' => getenv('DB_HOST'),
+    'port'     => getenv('DB_PORT'),
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
+    'database' => getenv('DB_NAME'),
     'charset'  => 'utf8mb4',
     'dbprefix' => '',
 ];
